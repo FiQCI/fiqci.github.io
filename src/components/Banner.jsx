43edd38ea866f74a5a665040a38fc6d13c0c25be
Joonas Nivala <joonas.nivala@gmail.com>
2025-03-04 14:47:22 +0200

    fix responsiveness

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Banner = ({ title }) => {
     return (
-        <div className='min-w-[375px] h-auto flex flex-col justify-center'>
+        <div className='h-auto flex flex-col justify-center'>
             <div className='justify-start landscape:justify-start sm:justify-start md:justify-start bg-[url(/assets/images/FiQCI-banner.jpg)] bg-center w-full h-[250px] flex flex-row items-center'>
                 <div className='mx-8 lg:mx-[100px]'>
                     <div className='bg-[#0D2B53] w-fit font-bold text-white leading-tight'>
