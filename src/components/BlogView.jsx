f727ba76feb84d5cd2ee0e90adc5cc9e461a0419
Joonas Nivala <joonas.nivala@gmail.com>
2025-03-07 10:10:00 +0200

    fetch blog content to react component

--- src/components/BlogView.jsx
+++ src/components/BlogView.jsx
@@ -1,32 +1,37 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import DOMPurify from 'dompurify'
 
-const BlogsBanner = () => (
-    <div className='min-w-[375px] h-auto flex flex-col justify-center'>
-        <div className='justify-start sm:justify-start md:justify-start bg-lumi bg-cover bg-center w-full h-[250px] flex flex-row items-center'>
-            <div className='mx-8 lg:mx-[100px]'>
-                <div className='bg-[#0D2B53] min-w-[300px] flex flex-row justify-center font-bold text-white leading-tight'>
-                    <h1 className='text-6xl px-10 py-10'>Blogs and instructions</h1>
-                </div>
-            </div>
-        </div>
-    </div>
-);
+const createMarkup = (htmlString) => {
+    const safeHTML = DOMPurify.sanitize(htmlString);
+    return { __html: safeHTML };
+};
 
-export const BlogView = ({ content }) => {
-    console.log(content)
+export const BlogView = () => {
+
+    const [content, setContent] = useState('');
+    const [metadata, setMetadata] = useState({});
+
+    useEffect(() => {
+        // Get content from data attribute
+        const contentDiv = document.getElementById('blog-content');
+        if (contentDiv) {
+            setContent(contentDiv.getAttribute('data-content'));
+        }
+
+        // Get metadata from JSON script
+        const metadataDiv = document.getElementById('blog-title');
+        if (metadataDiv) {
+            setMetadata(metadataDiv.getAttribute('data-content'));
+        }
+    }, []);
+    console.log(metadata)
     return (
         <div className='flex flex-col items-top mb-2'>
-            <BlogsBanner />
-            <div className='mx-8 lg:mx-[100px] flex lg:grid grid-cols-5 gap-8'>
-                <div className='mt-8 col-span-1 lg:sticky lg:top-16 lg:self-start z-10'>
-
+            <div className='mx-8 lg:mx-[100px] flex flex-col lg:grid grid-cols-5 gap-8'>
+                <div className='col-span-5'>
+                    
                 </div>
-                <div className='mt-8 md:py-0 col-span-3'>
-                <p>{content}</p>
-                </div>
-                <div className='mt-8 col-span-1'>
-
-                </div>
+                <div dangerouslySetInnerHTML={createMarkup(content)} className='mt-8 md:py-0 col-span-5' />
             </div>
         </div>
 
