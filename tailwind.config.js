6f83e1badb2a1b11198a26a3025f108b2ce48d90
Joonas Somero <joonas.somero@csc.fi>
2025-03-13 16:24:52 +0200

    Change font to Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: {
     files: ["./src/**/*.{js,jsx}", "./content/**/*.{md,html}"],
   },
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ['"Inter"', ...defaultTheme.fontFamily.sans],
+      },
       backgroundImage: {
         'fiqci': "var(--fiqci-banner-image)",
       },
