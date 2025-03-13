9b8369a96b189a30fab634286a66606ced373c3e
Joonas Somero <joonas.somero@csc.fi>
2025-03-13 16:33:02 +0200

    Move access card data to site constants

--- src/components/AccessCards.jsx
+++ src/components/AccessCards.jsx
@@ -4,6 +4,8 @@ import { CCard, CIcon, CCardContent } from '@cscfi/csc-ui-react';
 import { mdiArrowRight, mdiOpenInNew } from '@mdi/js';
 
 const AccessCardComponent = ({ title, teaser, description, links }) => {
+    const icon = link => link.external && mdiOpenInNew || mdiArrowRight
+
     return (
       <CCard className="flex flex-auto flex-col flex-wrap border border-gray-200 rounded-none shadow-md overflow-hidden hover:shadow-lg p-0 m-0 w-full bg-[#0D2B53] text-white">
         <img
@@ -21,7 +23,7 @@ const AccessCardComponent = ({ title, teaser, description, links }) => {
           <div className="flex flex-col space-y-2">
             {links.map((link, index) => (
               <a key={index} href={link.ref} className="text-sm font-bold hover:underline flex items-center gap-1">
-                {link.title} <CIcon path={link.icon} />
+                {link.title} <CIcon path={icon(link)} />
               </a>
             ))}
           </div>
@@ -31,7 +33,10 @@ const AccessCardComponent = ({ title, teaser, description, links }) => {
   };
   
 
-export const AccessCard = () => {
+export const AccessCards = () => {
+    const accessCardComponents =
+        SITE.constants.accessCards.map(card => (<AccessCardComponent {...card} />))
+
     return (
         <div className="mx-8 lg:mx-[100px] py-6">
             <div className="flex items-center justify-between mb-6">
@@ -39,48 +44,7 @@ export const AccessCard = () => {
             </div>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
-            <AccessCardComponent
-                    title="LUMI"
-                    description="The backbone of the classical HPC resources in FiQCI, and the portal for quantum computing resources, is the pan-European EuroHPC LUMI supercomputer. LUMI is the fastest and greenest supercomputer in Europe, hosted by CSC in Kajaani, Finland."
-                    teaser="/assets/images/FiQCI-banner.jpg"
-                    links={
-                      [
-                        {
-                          ref: "", title: "Getting started with Lumi", icon:mdiOpenInNew
-                        }
-                      ]
-                    }
-                />
-            <AccessCardComponent
-                    title="Helmi - VTT Q5"
-                    description="Helmi, the first Finnish quantum computer, co-developed by VTT and IQM Quantum Computers, is operated by VTT in Espoo, Finland. Helmi is based on superconducting technology, and presently provides five qubits. Upgrades to 20, then 50 qubits is planned for the near future."
-                    teaser="/assets/images/vtt-images/VTT_lab-2.jpg"
-                    links={
-                      [
-                        {
-                          ref: "", title: "How to access Helmi, instructions", icon:mdiArrowRight
-                        },
-                        {
-                          ref: "", title: "Read more about Helmi", icon:mdiOpenInNew
-                        }
-                      ]
-                    }
-                />
-            <AccessCardComponent
-                    title="LUMI"
-                    description="Kvasi, the Atos Quantum Learning Machine or Qaptiva is a quantum computing simulator with which you can learn to use and develop new quantum algorithms. Kvasi provides a platform for developing and simulating quantum algorithms in both ideal and realistic, noisy conditions. Kvasi is capable of simulating algorithms for quantum computers of 30+ qubits."
-                    teaser="/assets/images/FiQCI-banner.jpg"
-                    links={
-                      [
-                        {
-                          ref: "", title: "More about Kvasi", icon:mdiOpenInNew
-                        },
-                        {
-                          ref: "", title: "How to access Kvasi", icon:mdiOpenInNew
-                        }
-                      ]
-                    }
-                />
+            {accessCardComponents}
             </div>
             <div className="mt-4">
             <div className="flex items-center gap-3 text-md py-4">Other quantum resources will continuously be added to the FiQCI infrastructure.</div>
--- src/roots/accessCards.jsx
+++ src/roots/accessCards.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { AccessCard } from '../components/AccessCards'
+import { AccessCards } from '../components/AccessCards'
 import { injectComponent } from '../utils/root'
 
-const component = <AccessCard />
+const component = <AccessCards />
 const rootId = 'access-cards'
 
 injectComponent(component, rootId)
