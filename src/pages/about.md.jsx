e1025881c8844fd70bc5d09055388bf89e261e2c
Joonas Nivala <joonas.nivala@gmail.com>
2025-05-08 15:19:11 +0300

    testing

--- src/pages/about.md.jsx
+++ src/pages/about.md.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { createPortal } from 'react-dom'
 
-import { AboutFiqci } from '../components/AboutFiqci'
+import { AboutPage } from '../components/AboutPage'
 
 import { PageLayout } from '../components/layouts/page.html'
 
 import { useJsonApi } from '../hooks/useJsonApi'
 
 
-const AboutPage = () => {
+const AboutPageView = () => {
     const siteConstants = useJsonApi('api/site/constants.json')
 
     return <>
         <PageLayout {...siteConstants} title="About" />
         {createPortal(
-            <AboutFiqci />,
+            <AboutPage />,
             document.getElementById('about')
         )}
     </>
@@ -24,5 +24,5 @@ const AboutPage = () => {
 document.addEventListener('DOMContentLoaded', () => {
     const root = createRoot(document.getElementById('react-root'))
 
-    root.render(<AboutPage />)
+    root.render(<AboutPageView />)
 })
--- src/pages/default.jsx
+++ src/pages/default.jsx
@@ -1,21 +1,36 @@
 import React from 'react'
+import { createPortal } from 'react-dom'
+
 import { createRoot } from 'react-dom/client'
+
+import { PostLayout } from '../components/layouts/post.html'
 
 import { BaseLayout } from '../components/layouts/base.html'
 
 import { useJsonApi } from '../hooks/useJsonApi'
 
+import { BlogView } from '../components/BlogView'
 
-const DefaultPage = () => {
+
+const BlogViewPage = () => {
     const siteConstants = useJsonApi('api/site/constants.json')
 
-    return <>
-        <BaseLayout {...siteConstants} />
-    </>
+    if (!document.getElementById('blog-view')) {
+        return <>
+            <BaseLayout {...siteConstants}/>
+        </>
+    }
+
+    return (
+        <>
+            <PostLayout {...siteConstants} title="Blog" />
+            {createPortal(<BlogView />, document.getElementById('blog-view'))}
+        </>
+    )
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = createRoot(document.getElementById('react-root'))
 
-    root.render(<DefaultPage />)
+    root.render(<BlogViewPage />)
 })
