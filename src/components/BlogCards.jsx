742738eec58d38f618fb52fe0ee7408bf0f8925c
Joonas Nivala <joonas.nivala@csc.fi>
2025-06-05 20:08:37 +0300

    fix blog card to work with new filters

--- src/components/BlogCards.jsx
+++ src/components/BlogCards.jsx
@@ -4,20 +4,20 @@ import { CCard, CIcon, CCardContent } from '@cscfi/csc-ui-react';
 import { mdiArrowRight } from '@mdi/js';
 
 export const BlogCardComponent = props => {
-    const type = props?.filters?.Type ? Object.entries(props?.filters?.Type)?.filter(field => field[1])[0][0] : "News";
+    const type = props?.filters?.Type;
     return (
         <CCard className="flex flex-auto flex-col flex-wrap border border-gray-200 rounded-none shadow-md overflow-hidden hover:shadow-lg p-0 m-0 w-full"> {/* Adjusted card width */}
             <img src={props.teaser} alt="Logo" className="w-full h-28 scale-125 object-cover m-0 p-0" />
             <CCardContent className="flex flex-col border-none m-0">
                 <div>
-                    <a
-                        href={props.url.split(".")[0]}
-                        className="text-md text-black-500 hover:underline font-bold"
-                    >
-                        {props.title.length >= 89 ? props.title.slice(0, 90) + "..." : props.title}
+                    <a href={props.url.split(".")[0]}
+                        className="text-md text-black-500 hover:underline font-bold">
+                        <div>
+                            {props.title.length >= 89 ? props.title.slice(0, 90) + "..." : props.title}
+                        </div>
                     </a>
                     <p className="text-sm text-gray-500 pb-2 pt-1">
-                        { type } | {props.date}
+                        {type} | {props.date}
                     </p>
                 </div>
             </CCardContent>
@@ -28,9 +28,9 @@ export const BlogCardComponent = props => {
 export const BlogCards = () => {
     return (
         <div className="min-[2600px]:mx-auto min-[2600px]:max-w-[50vw]">
-            
+
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
-              { SITE.publications.slice(-5).reverse().map(blog => <BlogCardComponent {...blog} />) }
+                {SITE.publications.slice(-5).reverse().map(blog => <BlogCardComponent {...blog} />)}
             </div>
             <div className="mt-4">
                 <a
