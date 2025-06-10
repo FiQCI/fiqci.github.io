db96a3f14b274b390cc184f7fccd0a9fd3d11b02
Joonas Nivala <joonas.nivala@csc.fi>
2025-06-10 17:46:02 +0300

    adjuts number of cards

--- src/components/BlogCards.jsx
+++ src/components/BlogCards.jsx
@@ -29,8 +29,8 @@ export const BlogCards = () => {
     return (
         <div className="min-[2600px]:mx-auto min-[2600px]:max-w-[50vw]">
             
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
-              { SITE.publications.slice(-5).reverse().map(blog => <BlogCardComponent {...blog} />) }
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+              { SITE.publications.slice(-4).reverse().map(blog => <BlogCardComponent {...blog} />) }
             </div>
             <div className="mt-4">
                 <a
