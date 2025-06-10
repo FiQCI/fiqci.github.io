a041a71e65313c708450a3ee702bbb4ce7fa0625
Joonas Nivala <joonas.nivala@csc.fi>
2025-06-10 17:37:55 +0300

    adjust layout

--- src/components/ReadNext.jsx
+++ src/components/ReadNext.jsx
@@ -7,18 +7,15 @@ import { BlogCardComponent } from './BlogCards';
 export const ReadNext = ({ title }) => {
     return (
         <>
-            <div className='flex 2xl:hidden'>
+            <div className='flex lg:hidden'>
                 <div className='w-full'>
-                    <CAccordion value="Read next">
-                        <CAccordionItem heading="Read next" value="readnext">
-                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
-                                {SITE.publications.filter((blog) => blog.title !== title).slice(-5).reverse().map(blog => <BlogCardComponent {...blog} />)}
-                            </div>
-                        </CAccordionItem>
-                    </CAccordion>
+                    <h2 className='text-on-white pt-8 pb-4'>Read next</h2>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mb-10">
+                        {SITE.publications.filter((blog) => blog.title !== title).slice(-5).reverse().map(blog => <BlogCardComponent {...blog} />)}
+                    </div>
                 </div>
             </div>
-            <div className='hidden 2xl:block 2xl:sticky 2xl:top-[100px] bg-[#E5F2F8] px-10 py-5 ml-0 2xl:ml-10'>
+            <div className='hidden lg:block bg-[#E5F2F8] px-10 py-5 ml-0'>
                 <h3 className='!mb-10 !font-bold'>Read next:</h3>
                 {SITE.publications.filter((blog) => blog.title !== title).slice(-5).map((blog, index) => (
                     <div key={index}>
