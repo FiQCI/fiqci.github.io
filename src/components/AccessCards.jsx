cbfe8a2c3da2a6f3822165d3a8a43e2d38d4cbfe
Joonas Nivala <joonas.nivala@csc.fi>
2025-06-10 13:39:16 +0300

    edit spacing

--- src/components/AccessCards.jsx
+++ src/components/AccessCards.jsx
@@ -37,7 +37,7 @@ export const AccessCards = props => {
         props.cards?.map(card => (<AccessCardComponent {...card} key={card.title} />))
 
     return <>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-0 sm:gap-6 mb-2 sm:mb-0 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 mb-2 sm:mb-0 gap-[32px]">
             {accessCardComponents}
         </div>
         <div className="mt-4">
