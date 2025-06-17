9db8e86961f966cba5a28ab94785264a39bdf33a
Joonas Nivala <joonas.nivala@csc.fi>
2025-06-17 09:55:19 +0300

    404 page

---
+++ src/pages/404.md.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { createRoot } from 'react-dom/client'
+
+import { BaseLayout } from '../layouts/base.html'
+
+import { useJsonApi } from '../hooks/useJsonApi'
+
+const DefaultPage = () => {
+    console.log("moi")
+    const themeConstants = useJsonApi('api/theme/constants.json')
+    return <>
+        <BaseLayout {...themeConstants} />
+    </>
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = createRoot(document.getElementById('react-root'))
+
+    root.render(<DefaultPage />)
+})
