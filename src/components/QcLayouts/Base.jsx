c1fc871ddc113ef863c088f432a1b08f1c13d97d
Joonas Nivala <joonas.nivala@csc.fi>
2025-07-30 13:41:49 +0300

    remove import

--- src/components/QcLayouts/Base.jsx
+++ src/components/QcLayouts/Base.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { motion } from 'framer-motion';
 import { getColorForMetricValue } from '../../utils/generateGradient';
 import { formatMetricValue } from '../../utils/formatMetricValue';
