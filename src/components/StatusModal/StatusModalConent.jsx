ec31dc31cc04ebce5ae6ca75c271abeb578b7935
Joonas Nivala <joonas.nivala@csc.fi>
2026-01-07 13:37:00 +0200

    reorder qubit metrics

--- src/components/StatusModal/StatusModalConent.jsx
+++ src/components/StatusModal/StatusModalConent.jsx
@@ -51,23 +51,31 @@ export const ModalContent = (props) => {
 
     const qubitMetricOptions =
         [
-            { name: '1->0 MCM Error', value: 'measure_ssro_error_1_to_0', title: 'MCM = Mid Circuit Measurement' },
-            { name: '0->1 MCM Error', value: 'measure_ssro_error_0_to_1', title: 'MCM = Mid Circuit Measurement' },
-            { name: 'MCM Fidelity', value: 'measure_ssro_fidelity', title: 'MCM = Mid Circuit Measurement' },
+            //coherence times
+            { name: 'T1 Time', value: 't1_time' },
+            { name: 'T2 Time', value: 't2_time' },
+            { name: 'T2 Echo Time', value: 't2_echo_time' },
 
-            { name: 'QNDness Fidelity', value: 'measure_qndness_fidelity', title: 'QND = Quantum Non-Demolition'},
-            { name: 'QNDness 0 State', value: 'measure_qndness_qndness_0', title: 'QND = Quantum Non-Demolition'},
-            { name: 'QNDness 1 State', value: 'measure_qndness_qndness_1', title: 'QND = Quantum Non-Demolition'},
+            //gate fidelity
+            { name: 'PRX Gate Fidelity', value: 'prx_rb_fidelity' },
 
-
+            //Q50 readout metrics
+            //Helmi: does not exist
             { name: '1->0 Readout Error', value: 'measure_fidelity_ssro_error_1_to_0' },
             { name: '0->1 Readout Error', value: 'measure_fidelity_ssro_error_0_to_1' },
             { name: 'Readout Fidelity', value: 'measure_fidelity_ssro_fidelity' },
 
-            { name: 'T1 Time', value: 't1_time' },
-            { name: 'T2 Time', value: 't2_time' },
-            { name: 'T2 Echo Time', value: 't2_echo_time' },
-            { name: 'PRX Gate Fidelity', value: 'prx_rb_fidelity' },
+            //Q50: mid circuit measurement metrics
+            //Helmi: readout metrics
+            { name: '1->0 MCM Error', value: 'measure_ssro_error_1_to_0', title: 'MCM = Mid Circuit Measurement' },
+            { name: '0->1 MCM Error', value: 'measure_ssro_error_0_to_1', title: 'MCM = Mid Circuit Measurement' },
+            { name: 'MCM Fidelity', value: 'measure_ssro_fidelity', title: 'MCM = Mid Circuit Measurement' },
+
+            //Q50: destructiveness of measurement metrics
+            //Helmi: does not exist
+            { name: 'QNDness Fidelity', value: 'measure_qndness_fidelity', title: 'QND = Quantum Non-Demolition'},
+            { name: 'QNDness 0 State', value: 'measure_qndness_qndness_0', title: 'QND = Quantum Non-Demolition'},
+            { name: 'QNDness 1 State', value: 'measure_qndness_qndness_1', title: 'QND = Quantum Non-Demolition'},
 
         ]
 
