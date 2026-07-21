f6c29a53ddfad186911bcaadcd754adede47afc9
Joonas Nivala <joonas.nivala@csc.fi>
2026-07-21 15:08:55 +0300

    modify notification banners

--- src/components/ServiceStatus.jsx
+++ src/components/ServiceStatus.jsx
@@ -7,24 +7,7 @@ import { CCard, CCardTitle, CCardContent, CIcon, CButton, CSelect } from '@cscfi
 import { StatusModal } from './StatusModal/StatusModal';
 import { BookingModal } from './bookingCalendar.jsx';
 import { API_BASE_URL } from '../config/api.js';
-
-const ALERT_STYLES = {
-  warning: { bg: 'bg-orange-200', icon: mdiAlert },
-  error: { bg: 'bg-red-200', icon: mdiClose },
-  info: { bg: 'bg-sky-200', icon: mdiInformation },
-};
-
-const AlertBanner = ({ type, text }) => {
-  const { bg, icon } = ALERT_STYLES[type?.toLowerCase()] ?? ALERT_STYLES.info;
-  return (
-    <div className={`flex flex-row gap-4 w-full p-3 rounded-md ${bg} items-start sm:items-center`}>
-      <CIcon key={icon} path={icon} />
-      <p className='text-[16px]'>
-        {type ? text : 'Loading...'}
-      </p>
-    </div>
-  );
-};
+import { AlertBanner } from './AlertBanner.jsx';
 
 const StatusCard = (props) => {
   const isOnline = props.health;
